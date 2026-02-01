afcbf90c020cf0658d64944d1ee5905c10251592
Shaikh Mubarish <mubarishshaikh2@gmail.com>
2026-02-01 23:58:13 +0530

    add dataframe

--- step.py
+++ step.py
@@ -1,1 +1,9 @@
+class Solution:
+    def minimumCost(self, nums):
+        # First element always contributes to cost
+        first = nums[0]
 
+        # Get the two smallest elements from the rest
+        rest = sorted(nums[1:])
+
+        return first + rest[0] + rest[1]
