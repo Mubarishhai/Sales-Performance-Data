bda28ec1c6581fcc9b3f8d2b09ac65d9032441eb
Shaikh Mubarish <mubarishshaikh2@gmail.com>
2026-02-03 21:42:24 +0530

    add streamlit UI

---
+++ analysis.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+def load_and_clean_data(file_path):
+    df = pd.read_csv(file_path, encoding="latin1")
+
+    # 🔥 IMPORTANT FIX
+    df.columns = df.columns.str.strip().str.lower()
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+
+    df["sales"] = pd.to_numeric(df["sales"], errors="coerce")
+    df.dropna(subset=["sales"], inplace=True)
+
+    df["date"] = pd.to_datetime(df["date"], errors="coerce")
+    df.dropna(subset=["date"], inplace=True)
+
+    return df
+
+
+
+def calculate_kpis(df):
+    return {
+        "Total Sales": round(df["sales"].sum(), 2),
+        "Average Sales": round(df["sales"].mean(), 2),
+        "Max Sale": round(df["sales"].max(), 2),
+        "Min Sale": round(df["sales"].min(), 2),
+        "Total Orders": len(df)
+    }
+
+
+def sales_by_region(df):
+    return df.groupby("region")["sales"].sum().sort_values(ascending=False)
+
+
+def top_products(df, n=5):
+    return (
+        df.groupby("product")["sales"]
+        .sum()
+        .sort_values(ascending=False)
+        .head(n)
+    )
+
+
+def monthly_sales_trend(df):
+    df["month"] = df["date"].dt.to_period("M")
+    return df.groupby("month")["sales"].sum()
