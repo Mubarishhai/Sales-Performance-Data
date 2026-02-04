3fcd9ef5489aff7491bc0acf6497bcabe511061d
Shaikh Mubarish <mubarishshaikh2@gmail.com>
2026-02-04 23:45:39 +0530

    add streamlit UI

--- analysis.py
+++ analysis.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from step import load_and_clean_data  # 👈 import your cleaning function
 
+st.set_page_config(page_title="Sales Dashboard", layout="wide")
+
+@st.cache_data
+def load_data():
+    return load_and_clean_data("sales_data_sample.csv")
+
+df = load_data()
